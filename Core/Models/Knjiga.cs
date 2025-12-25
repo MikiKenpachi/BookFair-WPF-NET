9ca6d8b53645c4032450edf11fe1f8dcb0f7c06f
Boris <boris.stepanovic04@gmail.com>
2025-12-25 03:12:22 +0100

    Added functions for adding,updating,showing and deleting for Autor and added functions for showing and deleting Knjiga

--- Core/Models/Knjiga.cs
+++ Core/Models/Knjiga.cs
@@ -5,20 +5,20 @@ namespace SajamKnjigaProjekat.Core.Models
 {
     public class Knjiga
     {
-        public string ISBN;
-        public string Naziv;
-        public string Zanr;
-        public string Godina_izdanja;
-        public string Cena;
-        public string Broj_strana;
-        public List<Autor> ListaAutora= new List<Autor>();
-        public string Izdavac;
-        public List<string> Kupili;
-        public List<string> Na_listi_zelja;
+        public string ISBN { get; set; }
+        public string Naziv { get; set; }
+        public string Zanr { get; set; }
+        public string Godina_izdanja { get; set; }
+        public string Cena { get; set; }
+        public string Broj_strana { get; set; }
+        public List<Autor> ListaAutora { get; set; } = new List<Autor>();
+        public string Izdavac { get; set; }
+        public List<Posetilac> Kupili { get; set; } = new List<Posetilac>();
+        public List<Posetilac> Na_listi_zelja { get; set; } = new List<Posetilac>();
 
         public Knjiga() { }
 
-        public Knjiga(string iSBN, string naziv, string zanr, string godina_izdanja, string cena, string broj_strana, List<Autor> autori, string izdavac, List<string> kupili, List<string> na_Listi_Zelja)
+        public Knjiga(string iSBN, string naziv, string zanr, string godina_izdanja, string cena, string broj_strana, List<Autor> listaAutora, string izdavac, List<Posetilac> kupili, List<Posetilac> na_listi_zelja)
         {
             ISBN = iSBN;
             Naziv = naziv;
@@ -26,10 +26,29 @@ namespace SajamKnjigaProjekat.Core.Models
             Godina_izdanja = godina_izdanja;
             Cena = cena;
             Broj_strana = broj_strana;
-            ListaAutora = autori;
+            ListaAutora = listaAutora;
             Izdavac = izdavac;
             Kupili = kupili;
-            Na_listi_zelja = na_Listi_Zelja;
+            Na_listi_zelja = na_listi_zelja;
         }
+
+        public void DodajListuAutora(Autor autor)
+        {
+            if (!ListaAutora.Contains(autor))
+                ListaAutora.Add(autor);
+        }
+
+        public void DodajuKupili(Posetilac posetilac)
+        {
+            if (!Kupili.Contains(posetilac))
+                Kupili.Add(posetilac);
+        }
+        public void DodajuListuZelja(Posetilac posetilac)
+        {
+            if (!Na_listi_zelja.Contains(posetilac))
+                Na_listi_zelja.Add(posetilac);
+        }
+
+
     }
 }
